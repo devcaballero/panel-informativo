0f33bc0a8eff4c38a0ce7685621cf748380c9ab6
devcaballero <claudiocaballero.dev@gmail.com>
2023-08-17 04:13:36 -0300

    add johnny red

--- src/app/flores/flores.component.ts
+++ src/app/flores/flores.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-flores',
   templateUrl: './flores.component.html',
   styleUrls: ['./flores.component.css']
 })
-export class FloresComponent {
+export class FloresComponent implements OnInit {
+  johnnyred: string | undefined;
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    const url = 'https://price-webscraper.onrender.com/johnnyred';
+    this.apiService.getData(url).subscribe(
+      (data) => {
+        this.johnnyred = data;
+      },
+      (error) => {
+        console.log('Error al obtener el precio de johnny red 750ml:', error);
+      }
+    );
+  }
+  
 }
